b23f9416709136fec78c6d4788edcd50ad24593a
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-07-25 19:51:30 -0400

    Remove dead code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use bracket_lib::prelude::*;
 
 pub mod player;
@@ -11,10 +9,8 @@ pub struct Player {
 }
 
 struct State {
-    frame: usize,
     timer: f32,
     players: Vec<Player>,
-    key_state: HashMap<VirtualKeyCode, bool>
 }
 
 impl GameState for State {
@@ -23,9 +19,9 @@ impl GameState for State {
 
         player_input(ctx, player);
 
+        ctx.set_active_console(1);
         ctx.cls();
 
-        ctx.print(1, 1, "Watch them go!");
         ctx.printer(
             1,
             2,
@@ -34,11 +30,14 @@ impl GameState for State {
             None,
         );
 
+        ctx.set_active_console(0);
+        ctx.cls();
+
         ctx.add_sprite(
             Rect::with_size(player.x, player.y, 32, 32),
             400 - player.y,
             RGBA::from_f32(1.0, 1.0, 1.0, 1.0),
-            self.frame % 4,
+            0,
         );
 
         self.timer += ctx.frame_time_ms;
@@ -51,22 +50,21 @@ fn main() -> BError {
     let context = BTermBuilder::new()
         .with_sprite_console(640, 400, 0)
         .with_font("terminal8x8.png", 8, 8)
+        .with_simple_console_no_bg(80, 50, "terminal8x8.png")
         .with_title("Bracket Terminal - Sprite Console")
         .with_sprite_sheet(
             SpriteSheet::new("resources/sprite_dood.png")
                 .add_sprite(Rect::with_size(0, 0, 85, 132)),
         )
-        .with_vsync(false)
+        .with_vsync(true)
         .build()?;
 
     let mut players = Vec::new();
     players.push(Player { x: 120, y: 120 });
 
     let gs = State {
-        frame: 0,
         timer: 0.0,
         players,
-        key_state: HashMap::default(),
     };
 
     main_loop(context, gs)
